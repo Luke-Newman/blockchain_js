3ce21771de118c5a62e65827dbe539288ba0847e
luken <lukenewman@live.com.au>
2018-05-31 14:03:35 +1000

    added blockchain class

---
+++ blockchain.js
@@ -0,0 +1,16 @@
+const Block = require('./block');
+
+class Blockchain {
+    constructor() {
+        this.chain = [Block.genesis()];
+
+    }
+
+    addBlock(data) {
+        const lastBlock = this.chain[this.chain.length -1];
+        const block = Block.mineBlock(this.chain[this.chain.length -1], data);
+        this.chain.push(block);
+    }
+}
+
+module.exports = Blockchain;
